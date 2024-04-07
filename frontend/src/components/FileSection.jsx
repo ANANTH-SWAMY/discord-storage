342406a95cfa69e8a9fda223d1383e68449a78b5
ANANTH-SWAMY <ananth.gnr@gmail.com>
2024-04-07 21:17:15 +0530

    added message for no files

--- frontend/src/components/FileSection.jsx
+++ frontend/src/components/FileSection.jsx
@@ -3,11 +3,13 @@ import Card from "./Card"
 function FileSection({files, setFiles}){
 	return(
 		<div className="flex items-center justify-center flex-wrap gap-8 w-full grow">
-			{files.map((i, index) => {
+			{files.length!=0 && files.map((i, index) => {
 				return(
 					<Card fileName={i.fileName} randName={i.randName} setFiles={setFiles} key={index}/>
 				)
 			})}
+
+			{files.length==0 && <div className="text-4xl">No Files Found</div>}
 		</div>
 	)
 }
