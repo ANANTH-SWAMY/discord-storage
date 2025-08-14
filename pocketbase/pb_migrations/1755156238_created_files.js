b7c21b76fe43d5d23d7ae9eeee6a0f181384fb60
ANANTH-SWAMY <ananth.gnr@gmail.com>
2025-08-14 12:55:04 +0530

    migrations

---
+++ pocketbase/pb_migrations/1755156238_created_files.js
@@ -0,0 +1,87 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = new Collection({
+    "createRule": "",
+    "deleteRule": null,
+    "fields": [
+      {
+        "autogeneratePattern": "[a-z0-9]{15}",
+        "hidden": false,
+        "id": "text3208210256",
+        "max": 15,
+        "min": 15,
+        "name": "id",
+        "pattern": "^[a-z0-9]+$",
+        "presentable": false,
+        "primaryKey": true,
+        "required": true,
+        "system": true,
+        "type": "text"
+      },
+      {
+        "hidden": false,
+        "id": "json1893377944",
+        "maxSize": 0,
+        "name": "ids",
+        "presentable": false,
+        "required": false,
+        "system": false,
+        "type": "json"
+      },
+      {
+        "autogeneratePattern": "",
+        "hidden": false,
+        "id": "text2620999259",
+        "max": 0,
+        "min": 0,
+        "name": "fileName",
+        "pattern": "",
+        "presentable": false,
+        "primaryKey": false,
+        "required": false,
+        "system": false,
+        "type": "text"
+      },
+      {
+        "autogeneratePattern": "",
+        "hidden": false,
+        "id": "text663732054",
+        "max": 0,
+        "min": 0,
+        "name": "randName",
+        "pattern": "",
+        "presentable": false,
+        "primaryKey": false,
+        "required": false,
+        "system": false,
+        "type": "text"
+      },
+      {
+        "hidden": false,
+        "id": "number375098648",
+        "max": null,
+        "min": null,
+        "name": "noOfFiles",
+        "onlyInt": false,
+        "presentable": false,
+        "required": false,
+        "system": false,
+        "type": "number"
+      }
+    ],
+    "id": "pbc_3446931122",
+    "indexes": [],
+    "listRule": "",
+    "name": "files",
+    "system": false,
+    "type": "base",
+    "updateRule": "",
+    "viewRule": ""
+  });
+
+  return app.save(collection);
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_3446931122");
+
+  return app.delete(collection);
+})
